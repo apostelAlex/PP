115efb232c6f2908cc3cdeccb1cb283530ab7efc
Alexandros Apostolidis <df278@stud.uni-heidelberg.de>
2022-09-17 18:11:09 +0200

    *.java~ files removed

--- zettel/1/vorgaben/.#IOTools.java
+++
@@ -1,1 +0,0 @@
-a2@mobile-232-237.wlan.uni-mannheim.de.46231
---
+++ zettel/1/vorgaben/test.java
@@ -0,0 +1,10 @@
+public class test{
+    public static void main(String [] args){
+	int i = 36;
+	i/=10;
+	System.out.println(i);
+	
+    }
+
+
+    }
