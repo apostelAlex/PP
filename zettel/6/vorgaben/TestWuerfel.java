54e11a1616abb659ecf45db4d563723d6fee928e
Alexandros Apostolidis <df278@stud.uni-heidelberg.de>
2022-10-22 21:32:11 +0200

    Aufgabe 2 done

--- zettel/6/vorgaben/TestWuerfel.java
+++ zettel/6/vorgaben/TestWuerfel.java
@@ -6,6 +6,22 @@ public class TestWuerfel {
         
         /* *** Aufgabenteil (c) *** */
         // 2
+        wuerfel[0] = new ZahlenWuerfel(6);
+        wuerfel[1] = new ZahlenWuerfel(6);
+        wuerfel[2] = new ZahlenWuerfel(12);
+        wuerfel[3] = new ZahlenWuerfel(12);
+        wuerfel[4] = new Farbwürfel();
+        wuerfel[5] = new Farbwürfel();
+        for(int i=0; i<k; i++){
+            System.out.println(i+". Wurf");
+            for(Wuerfel w : wuerfel){
+                w.werfen();
+            }
+            for(int j=0; j< wuerfel.length; j++) {
+                System.out.println(wuerfel[j].toString());
+            }
+            System.out.println();
+        }
         
     }
 }
--- zettel/6/vorgaben/Wuerfel.java
+++ zettel/6/vorgaben/Wuerfel.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Wuerfel {
     private int nr;
 
@@ -14,9 +16,45 @@ public class Wuerfel {
 
 /* *** Aufgabenteil (a) *** */
 // 4
+class ZahlenWuerfel extends Wuerfel{
+    int n_seiten;
+    int wert;
+    private static int count=1;
+
+    public ZahlenWuerfel(int n_seiten){
+        super(count);
+        this.n_seiten = n_seiten;
+        count++;
+    }
+    public void werfen(){
+        Random rand = new Random();
+        wert = rand.nextInt(n_seiten)+1 ;
+    }
+    public String toString(){
+        return "Zahlen-"+super.toString() + " ("+n_seiten+" Seiten) -> "+ wert;
+    }
+
+}
 
 
 
 /* *** Aufgabenteil (b) *** */
 // 4
+class Farbwürfel extends Wuerfel{
+    static String[] farben = {"blau", "rot", "gruen", "gelb"};
+    String farbe;
+    static int count = 1;
+    public Farbwürfel(){
+        super(count);
+        count++; 
+    }
+    public void werfen(){
+        Random rand = new Random();
+        farbe = farben[rand.nextInt(farben.length)];
+    }
+    public String toString(){
+        return "Farb-" + super.toString() + " -> " + farbe;
+    }
+    
+}
 
